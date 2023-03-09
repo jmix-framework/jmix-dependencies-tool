3d6dd318d61fa25f355f364b8f7c1356696d8b0c
Dmitriy Kremnev <kremnev.dima@mail.ru>
2023-03-09 17:05:13 +0400

    Add --report-file command option for the export command ( jmix-framework/jmix-dependencies-tool#6)

--- deptool/src/main/java/io/jmix/dependency/cli/command/ExportCommand.java
+++ deptool/src/main/java/io/jmix/dependency/cli/command/ExportCommand.java
@@ -5,9 +5,11 @@ import com.beust.jcommander.Parameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.TreeSet;
 
 @Parameters(commandDescription = "Exports resolved dependencies")
 public class ExportCommand implements BaseCommand {
@@ -20,6 +22,9 @@ public class ExportCommand implements BaseCommand {
     @Parameter(names = {"--gradle-user-home"}, description = "Gradle user home directory for resolver project")
     private String gradleUserHome;
 
+    @Parameter(names = {"--report-file"}, description = "Path to the report file")
+    private String reportFile;
+
     @Override
     public void run() {
         if (gradleUserHome == null) {
@@ -31,6 +36,12 @@ public class ExportCommand implements BaseCommand {
 
         log.info("Target directory: {}", Paths.get(targetDirectory).toAbsolutePath().normalize());
         log.info("Gradle user home directory: {}", Paths.get(gradleUserHome).toAbsolutePath().normalize());
+
+        TreeSet<String> exportedDependencies = new TreeSet<>();
+
+        if (reportFile != null) {
+            log.info("Path to the report file: {}", Paths.get(reportFile).toAbsolutePath().normalize());
+        }
 
         Path gradleUserHomeDir = Paths.get(gradleUserHome);
         Path cachedGradleArtifactsDir = gradleUserHomeDir.resolve("caches/modules-2/files-2.1");
@@ -49,6 +60,15 @@ public class ExportCommand implements BaseCommand {
 
                         String nestedOutputDirPath = moduleDir.getFileName().toString().replace(".", "/") + "/" +
                                 artifactDir.getFileName().toString() + "/" + versionDir.getFileName().toString();
+
+                        if (reportFile != null && file.getFileName().toString().endsWith(".jar")) {
+                            String dependency = moduleDir.getFileName() +
+                                    ":" + artifactDir.getFileName() +
+                                    ":" + versionDir.getFileName() +
+                                    "\n";
+
+                            exportedDependencies.add(dependency);
+                        }
 
                         try {
                             Path outputDirectory = targetDirectoryPath.resolve(nestedOutputDirPath);
@@ -69,6 +89,23 @@ public class ExportCommand implements BaseCommand {
         } catch (IOException e) {
             throw new RuntimeException("Error on copying files from gradle caches", e);
         }
+
+        if (!exportedDependencies.isEmpty()) {
+            try {
+                Files.deleteIfExists(Paths.get(reportFile));
+            } catch (IOException e) {
+                throw new RuntimeException("Error while deleting an existing report file", e);
+            }
+
+            try (FileWriter fileWriter = new FileWriter(reportFile, true)) {
+                for (String exportedDependency : exportedDependencies) {
+                    fileWriter.append(exportedDependency);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Error while writing the report file", e);
+            }
+        }
+
         log.info("Export completed successfully");
     }
 }
